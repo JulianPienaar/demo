3566540dcf4685513daeab743df3d71756c9e79f
JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>
2023-03-21 16:44:52 +0200

    Added ionic project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router : Router) {}
 
+  logout(){
+    this.router.navigate(['/login']);
+  }
+  like(){
+    console.log("Liked post");
+  }
+  dislike(){
+    console.log("Disliked post");
+  }
+  comment(){
+    console.log("Commented on post");
+  }
+  bid(){
+    console.log("Bid on post");
+  }
 }
